80f7a6ca2bd96b8ed95cfb12c760c5e84abb3c76
LINKZEN-PC\mssuh <mssuh21c#gmail.com>
2017-09-21 10:21:16 +0900

    소스트리 공부중 , 여러개의 프로젝트를 한 곳에서 커밋 , 푸시 .... 할 수있다는 것을 알았다.

--- app/src/main/java/com/example/user/may26/MainActivity.java
+++ app/src/main/java/com/example/user/may26/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -35,6 +36,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
 
 
+        Log.d("markDebug", " this is onCrete in May26");
+        
 /*
         mBtn1.setOnClickListener ( new  Button.OnClickListener()  {
                     public void onClick ( View v ) {
